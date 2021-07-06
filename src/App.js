4c70d6aff38f4fdf5f97150a205b792a88639e1a
Deej <deejpotter@gmail.com>
2021-07-07 00:29:50 +1000

    Mostly added the authentication.

--- src/App.js
+++ src/App.js
@@ -1,29 +1,25 @@
-import React, { useState } from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter, Route, Switch, Redirect, withRouter } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
 import Home from './components/Home';
 import Footer from './components/Footer';
 import Trivia from './components/Trivia';
 import Answers from './components/Answers';
-import Login from './components/login';
+import netlifyIdentity from 'netlify-identity-widget';
 
 function App() {
-  const [token, setToken] = useState();
-  if (!token) {
-    return <Login setToken={setToken} />
-  }
   return (
     <div>
       <BrowserRouter>
         <Header />
+        <AuthButton />
         <Switch>
           <Route path="/trivia">
             <Trivia />
           </Route>
-          <Route path="/answers">
-            <Answers />
-          </Route>
+          <PrivateRoute path="/answers" component={Answers} />
+          <Route path="/login" component={Login} />
           <Route path="/">
             <Home />
           </Route>
@@ -32,6 +28,89 @@ function App() {
       </BrowserRouter>
     </div>
   );
+}
+
+const netlifyAuth = {
+  isAuthenticated: false,
+  user: null,
+  authenticate(callback) {
+    this.isAuthenticated = true;
+    netlifyIdentity.open();
+    netlifyIdentity.on('login', user => {
+      this.user = user;
+      callback(user);
+    });
+  },
+  signout(callback) {
+    this.isAuthenticated = false;
+    netlifyIdentity.logout();
+    netlifyIdentity.on('logout', () => {
+      this.user = null;
+      callback();
+    });
+  }
+};
+
+const AuthButton = withRouter(
+  ({ history }) =>
+    netlifyAuth.isAuthenticated ? (
+      <p>
+        Welcome!{' '}
+        <button
+          onClick={() => {
+            netlifyAuth.signout(() => history.push('/'));
+          }}
+        >
+          Sign out
+        </button>
+      </p>
+    ) : (
+      <p>You are not logged in.</p>
+    )
+);
+
+function PrivateRoute({ component: Component, ...rest }) {
+  return (
+    <Route
+      {...rest}
+      render={props =>
+        netlifyAuth.isAuthenticated ? (
+          <Component {...props} />
+        ) : (
+          <Redirect
+            to={{
+              pathname: '/login',
+              state: { from: props.location }
+            }}
+          />
+        )
+      }
+    />
+  );
+}
+
+class Login extends React.Component {
+  state = { redirectToReferrer: false };
+
+  login = () => {
+    netlifyAuth.authenticate(() => {
+      this.setState({ redirectToReferrer: true });
+    });
+  };
+
+  render() {
+    let { from } = this.props.location.state || { from: { pathname: '/' } };
+    let { redirectToReferrer } = this.state;
+
+    if (redirectToReferrer) return <Redirect to={from} />;
+
+    return (
+      <div>
+        <p>You must log in to view the page at {from.pathname}</p>
+        <button onClick={this.login}>Log in</button>
+      </div>
+    );
+  }
 }
 
 export default App;
