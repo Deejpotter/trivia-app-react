a5dda7881eaf320ec1a8808595d376f611b10a52
Deejpotter <deejpotter@gmail.com>
2021-07-06 11:38:34 +1000

    Updated some stuff

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,13 +8,17 @@ function Header() {
                 <div className="container">
                     <div className="brand">
                         <a href="">
-                            <img src="{{ '/images/logo.png' | url }}" />
+                            <img src="" />
                         </a>
                     </div>
                     <nav>
                         <label htmlFor="show-menu">Show Menu</label>
                         <input type="checkbox" id="show-menu" role="button" />
                         <ul id="menu">
+                            <Navlink content="Home" location="/" />
+                            <Navlink content="Home" location="/" />
+                            <Navlink content="Home" location="/" />
+                            <Navlink content="Home" location="/" />
                             <Navlink content="Home" location="/" />
                         </ul>
                     </nav>
