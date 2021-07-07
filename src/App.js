3a3dbc604190ea3c0d5128f44d1b6fed5b1de120
Deejpotter <deejpotter@gmail.com>
2021-07-07 21:32:46 +1000

    Tried to update the login function

--- src/App.js
+++ src/App.js
@@ -75,17 +75,16 @@ class Login extends React.Component {
 
   login = () => {
     netlifyAuth.authenticate(() => {
-      this.setState({ redirectToReferrer: true });
-      netlifyIdentity.refresh().then();
+      this.setState({ redirectToReferrer: true }) 
     });
   };
-  
+
   render() {
     let { from } = this.props.location.state || { from: { pathname: '/' } };
     let { redirectToReferrer } = this.state;
-    
+
     if (redirectToReferrer) return <Redirect to={from} />;
-    
+
     return (
       <div>
         <p>You must log in to view the page at {from.pathname}</p>
